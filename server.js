03eeceaee7eb80f6d8218c47676a7224a5e9ff64
Ben Kasper <bckasper@gmail.com>
2021-10-10 21:30:51 -0400

    added a variable for the db.json file; added middleware for static files; added code for body parsing

--- server.js
+++ server.js
@@ -2,7 +2,16 @@
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
+const notesDB = require('./db/db.json')
 
 // Initialize PORT and Express Application
 const PORT = process.env.PORT || 3001;
-const app = express()
+const app = express();
+
+// Static middleware for the public folder
+app.use(express.static('public'));
+app.get('/', (request, response) => response.sendFile(path.join(__dirname, '/public/')));
+
+// Body Parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
