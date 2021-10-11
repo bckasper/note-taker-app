a8278346915705be1eac1c989976371bd38a68c6
Ben Kasper <bckasper@gmail.com>
2021-10-10 21:25:55 -0400

    initialized PORT and express App variables

--- server.js
+++ server.js
@@ -3,3 +3,6 @@ const express = require('express')
 const path = require('path')
 const fs = require('fs')
 
+// Initialize PORT and Express Application
+const PORT = process.env.PORT || 3001;
+const app = express()
