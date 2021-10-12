a92e362d0eced459fac3122dae0f46aee129e01b
Ben Kasper <bckasper@gmail.com>
2021-10-11 20:54:33 -0400

    added a get api for rendering the existing notes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
-const notesDB = require('./db/db.json')
+const notesDB = require('./db/db.json');
 
 // Initialize PORT and Express Application
 const PORT = process.env.PORT || 3000;
@@ -25,8 +25,15 @@ app.get('/notes', (request, response) => {
     response.sendFile(path.join(__dirname, 'public/notes.html'))
 })
 
+// Get API for reading the current notes
+app.get('/api/notes', (request, response) => {
+    fs.readFile('./db/db.json', (error, notes) => {
+        if(error){console.log(error)}
+                response.json(JSON.parse(notes))
+    })
+})
 
-// Listening
+// Listening for application
 app.listen(PORT, () => {
     console.log(`Note Taker app is listening on PORT ${PORT}`)
 })
