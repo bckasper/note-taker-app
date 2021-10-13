20371268e2d922215c0fbc39a8a88d00fbd0ebc6
Ben Kasper <bckasper@gmail.com>
2021-10-12 20:55:29 -0400

    completed a delete api

--- server.js
+++ server.js
@@ -79,6 +79,64 @@ app.post('/api/notes', (request, response) => {
 
 })
 
+// POST API for deleting the entered note
+app.delete('/api/notes/:id', (request, response) => {
+
+    // ID of the item we want to delete
+    targetID = request.params.id
+    // console.log(`Deleting note with ID ${targetID}`)
+    
+    // Getting existing notes first
+    fs.readFile('./db/db.json', 'utf8', (error, data) => {
+        if(error) {
+            console.log(error)
+        } else {
+
+            let notes = JSON.parse(data)
+            let filteredNotes = []
+
+            for(let i = 0; i < notes.length; i++){
+                if(targetID !== notes[i].id){
+                    filteredNotes.push(notes[i])
+                }
+            }
+
+            
+            console.log(notes)
+            console.log(notes.length)
+            console.log(notes[0].id)
+            
+
+
+
+
+            // console.log(typeof data)
+            // console.log(data)
+            // console.log(filteredNotes[0])
+            // // const target = filteredNotes.indexOf(targetID)
+            // // console.log(`Target: ${target}`)
+            // filteredNotes.splice(target, 1 )
+            
+            
+            // Re-write the file without the target item
+            fs.writeFile('./db/db.json', JSON.stringify(filteredNotes), (error) => {
+                if(error){
+                    console.log(error)
+                } else {
+                    console.log(`Notes have been updated`)
+                    response.end()
+                }
+            })
+
+        }
+    })    
+
+
+
+    
+
+})
+
 // Listening for application
 app.listen(PORT, () => {
     console.log(`Note Taker app is listening on PORT ${PORT}`)
